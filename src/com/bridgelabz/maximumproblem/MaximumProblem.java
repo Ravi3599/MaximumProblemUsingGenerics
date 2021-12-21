bf6191ed10b3a3a787c9d45de66f8c821d58af02
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-21 13:54:05 +0530

    UPDATED: maximum amongst three Integers

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -1,7 +1,32 @@
 package com.bridgelabz.maximumproblem;
 
+import java.util.Scanner;
+
 public class MaximumProblem {
+	static Integer firstNumber,secondNumber,thirdNumber;
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Maximum Problem Using Generics");
+		getInput();
+		testMaximum(firstNumber,secondNumber,thirdNumber);
+		
+	}
+	public static void getInput() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first Number");
+		firstNumber = sc.nextInt();
+		System.out.println("Enter second Number");
+		secondNumber = sc.nextInt();
+		System.out.println("Enter third Number");
+		thirdNumber = sc.nextInt();
+	}
+	public static void testMaximum(Integer x,Integer y,Integer z) {
+		Integer max = x;
+		if(y.compareTo(max) > 0){
+			max = y;
+		}
+		if(z.compareTo(max) > 0){
+			max = z;
+		}
+		System.out.println(max+" is maximum number");
 	}
 }
